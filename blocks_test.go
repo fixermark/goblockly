a53a705359c071a7e8930e958dbe0c10ff190d35
Mark Tomczak <fixermark@gmail.com>
2015-12-20 20:56:40 -0500

    Tests for blocks.go

--- blocks_test.go
+++ blocks_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 )
 
+var interp *Interpreter = &Interpreter{}
+
 func stringsEqual(t *testing.T, a, b string) {
 	if a != b {
 		_, file, line, ok := runtime.Caller(1)
@@ -113,15 +115,6 @@ func TestDeserializeBlock(t *testing.T) {
 		t.Fatal("Could not deserialize block: ", err)
 	}
 
-	i := &Interpreter{
-		nil,
-		func(s string) {
-			t.Errorf("Interpreter failed: %s", s)
-		},
-		nil,
-		nil,
-	}
-
 	stringsEqual(t, unmarshaled.XMLName.Space, "http://www.w3.org/1999/xhtml")
 	stringsEqual(t, unmarshaled.XMLName.Local, "xml")
 	intsEqual(t, len(unmarshaled.Blocks), 1)
@@ -130,20 +123,106 @@ func TestDeserializeBlock(t *testing.T) {
 	stringsEqual(t, loopBlock.X, "76")
 	stringsEqual(t, loopBlock.Y, "-1861")
 
-	varField := unmarshaled.Blocks[0].SingleFieldWithName(i, "VAR")
+	varField := unmarshaled.Blocks[0].SingleFieldWithName(interp, "VAR")
 	stringsEqual(t, varField, "i")
 
-	fromBlock := unmarshaled.Blocks[0].SingleBlockValueWithName(i, "FROM")
+	fromBlock := unmarshaled.Blocks[0].SingleBlockValueWithName(interp, "FROM")
 	stringsEqual(t, fromBlock.Type, "math_number")
-	stringsEqual(t, fromBlock.SingleFieldWithName(i, "NUM"), "0")
+	stringsEqual(t, fromBlock.SingleFieldWithName(interp, "NUM"), "0")
 
-	body := loopBlock.SingleBlockStatementWithName(i, "DO")
+	body := loopBlock.SingleBlockStatementWithName(interp, "DO")
 	stringsEqual(t, body.Type, "text_print")
 	body2 := body.Next
 	stringsEqual(t, body2.Type, "controls_flow_statements")
 
 	ifBlock := loopBlock.Next
 	intsEqual(t, ifBlock.Mutation.Else, 1)
-	ifBlock.SingleBlockValueWithName(i, "IF0")
+	ifBlock.SingleBlockValueWithName(interp, "IF0")
+
+}
+
+func checkFailInterpreter(t *testing.T, test func()) {
+	defer func() {
+		if r := recover(); r != nil {
+			return
+		}
+	}()
+	test()
+
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file = "UNKNOWN"
+		line = 0
+	}
+	t.Errorf("%s:%d - Failure not called on interpreter.", file, line)
+}
+
+func TestFailToInterpretBlock(t *testing.T) {
+	block := Block{
+		Type: "test",
+		Values: []BlockValue{
+			{
+				Name:   "empty",
+				Blocks: nil,
+			},
+			{
+				Name: "foo",
+				Blocks: []Block{
+					{
+						Type: "found",
+					},
+				},
+			},
+		},
+		Fields: []BlockField{
+			{
+				Name:  "bar",
+				Value: "barvalue",
+			},
+		},
+		Statements: []BlockStatement{
+			{
+				Name:   "empty",
+				Blocks: nil,
+			},
+			{
+				Name: "baz",
+				Blocks: []Block{
+					{
+						Type: "foundStatement",
+					},
+				},
+			},
+		},
+	}
+
+	val := block.SingleFieldWithName(interp, "bar")
+	stringsEqual(t, val, "barvalue")
+
+	checkFailInterpreter(t, func() {
+		block.SingleFieldWithName(interp, "does_not_exist")
+	})
+
+	val2 := block.SingleBlockValueWithName(interp, "foo")
+	stringsEqual(t, val2.Type, "found")
+
+	checkFailInterpreter(t, func() {
+		block.SingleBlockValueWithName(interp, "empty")
+	})
+
+	checkFailInterpreter(t, func() {
+		block.SingleBlockValueWithName(interp, "does_not_exist")
+	})
+
+	val3 := block.SingleBlockStatementWithName(interp, "baz")
+	stringsEqual(t, val3.Type, "foundStatement")
+
+	checkFailInterpreter(t, func() {
+		block.SingleBlockStatementWithName(interp, "empty")
+	})
+
+	checkFailInterpreter(t, func() {
+		block.SingleBlockStatementWithName(interp, "does_not_exist")
+	})
 
 }
